d96f721f0e81d5bb7df45994b49d3e84ef4f6fe4
Manraj Singh <manrajs2025@gmail.com>
2024-08-17 01:07:59 -0600

    Install dotenv and create boilerplate server

--- v2/server/src/index.ts
+++ v2/server/src/index.ts
@@ -1,1 +1,20 @@
-console.log('Boilerplate')
+import dotenv from 'dotenv'
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+
+async function startServer(): Promise<void> {
+  dotenv.config()
+
+  const server = new ApolloServer({
+    typeDefs: '',
+    resolvers: {},
+  })
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: parseInt(process.env.SERVER_PORT || '') },
+  })
+
+  console.log(`🛡️ Server ready at: ${url}`)
+}
+
+startServer()
