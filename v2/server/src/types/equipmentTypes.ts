1e883d3570f62a23be57f704443408be8764383b
Manraj Singh <manrajs2025@gmail.com>
2024-08-17 18:31:31 -0600

    Consolidate query types

--- v2/server/src/types/equipmentTypes.ts
+++ v2/server/src/types/equipmentTypes.ts
@@ -1,12 +1,19 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
-export interface MonsterApiResponse {
+interface EquipmentPropertyApiResponse {
+  readonly attack: number
+  readonly defense: number
+  readonly effect: string
+  readonly type: string
+}
+
+export interface EquipmentApiResponse {
   readonly id: number
   readonly name: string
   readonly category: string
   readonly description: string
   readonly image: string
   readonly common_locations: readonly string[] | null
-  readonly drops: readonly string[] | null
+  readonly properties: EquipmentPropertyApiResponse
   readonly dlc: boolean
 }
