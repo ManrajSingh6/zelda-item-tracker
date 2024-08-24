b6e111feaf620f607467e2c552e76fa53c7da380
Manraj Singh <manrajs2025@gmail.com>
2024-08-23 23:46:07 -0600

    Add hook for fetching monsters

---
+++ v2/client/src/hooks/queries/useFetchMonsters.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import { ApolloError, useQuery } from '@apollo/client'
+import {
+  MonsterFragment,
+  MonstersQuery,
+  MonstersQueryVariables
+} from '../../graphql/gen/gen-types'
+import { ALL_MONSTERS_QUERY } from '../../graphql/queries/monstersQueries'
+
+interface UseFetchMonstersReturn {
+  readonly monsters: readonly MonsterFragment[]
+  readonly loading: boolean
+  readonly error: ApolloError | undefined
+}
+
+export function useFetchMonsters(): UseFetchMonstersReturn {
+  const [monsters, setMonsters] = useState<readonly MonsterFragment[]>([])
+
+  const { data, loading, error } = useQuery<
+    MonstersQuery,
+    MonstersQueryVariables
+  >(ALL_MONSTERS_QUERY)
+
+  useEffect(() => {
+    // Only update data if it exists, otherwise keep the last success state
+    if (data?.monsters) {
+      setMonsters(data.monsters)
+    }
+  }, [data])
+
+  return {
+    monsters,
+    loading,
+    error
+  }
+}
