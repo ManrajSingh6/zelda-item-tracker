1fe143f542275739506d73efd6d12fe2b5faeea6
Manraj Singh <manrajs2025@gmail.com>
2024-08-23 23:31:10 -0600

    Create apollo client and wrap app in provider

--- v2/client/src/main.tsx
+++ v2/client/src/main.tsx
@@ -1,13 +1,21 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { BrowserRouter } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const apolloClient = new ApolloClient({
+  uri: import.meta.env.GRAPHQL_SERVER_URL ?? '',
+  cache: new InMemoryCache()
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ApolloProvider client={apolloClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </StrictMode>
 )
