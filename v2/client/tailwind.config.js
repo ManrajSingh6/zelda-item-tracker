0655ef5f22c1cb387203d612c739f2100f88a491
Manraj Singh <manrajs2025@gmail.com>
2024-08-23 22:52:52 -0600

    Install custom font and add to tailwind config

--- v2/client/tailwind.config.js
+++ v2/client/tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        hylianGreen: '#0E7147',
+        triforceGold: '#F2C335',
+        goronRed: '#A2231D',
+        zoraBlue: '#3A92D2',
+        shadowBlack: '#1A1A1A',
+        royalPurple: '#6A287E',
+        fairyPink: '#E899BC',
+        ancientStone: '#B5A897',
+        primaryBlue: '#0ea5e9',
+        darkGray: '#1e293b',
+        lightGray: '#f1f5f9',
+        white: '#ffffff',
+        accentIndigo: '#6366f1',
+        hoverTeal: '#14b8a6'
+      },
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif']
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
