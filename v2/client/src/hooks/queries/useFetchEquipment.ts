c665bb7a9345340376a87a43def823a852b57b45
Manraj Singh <manrajs2025@gmail.com>
2024-08-31 17:28:33 -0600

    Add hook to fetch equipment

---
+++ v2/client/src/hooks/queries/useFetchEquipment.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import { ApolloError, useQuery } from '@apollo/client'
+import {
+  EquipmentFragment,
+  EquipmentQuery,
+  EquipmentQueryVariables
+} from '../../graphql/gen/gen-types'
+import { ALL_EQUIPMENT_QUERY } from '../../graphql/queries/itemQueries'
+
+interface UseFetchEquipmentReturn {
+  readonly equipment: readonly EquipmentFragment[]
+  readonly loading: boolean
+  readonly error: ApolloError | undefined
+}
+
+export function useFetchEquipment(): UseFetchEquipmentReturn {
+  const [equipment, setEquipment] = useState<readonly EquipmentFragment[]>([])
+
+  const { data, loading, error } = useQuery<
+    EquipmentQuery,
+    EquipmentQueryVariables
+  >(ALL_EQUIPMENT_QUERY)
+
+  useEffect(() => {
+    // Only update data if it exists, otherwise keep the last success state
+    if (data?.equipment) {
+      setEquipment(data.equipment)
+    }
+  }, [data])
+
+  return {
+    equipment,
+    loading,
+    error
+  }
+}
