0e1fcb36c450127e7afb6cb27a5d7170399618f1
Manraj Singh <manrajs2025@gmail.com>
2024-08-31 15:45:09 -0600

    Update styles for grid and add loading component

--- v2/client/src/components/common/grid.tsx
+++ v2/client/src/components/common/grid.tsx
@@ -1,11 +1,24 @@
+import { LoadSpinner } from './loadSpinner'
+
 interface GridProps {
   readonly elements: readonly JSX.Element[]
+  readonly elementsLoading?: boolean
 }
 
-export function Grid({ elements }: GridProps): JSX.Element {
+export function Grid({ elements, elementsLoading }: GridProps): JSX.Element {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-4'>
-      {elements}
+    <div
+      className={`h-full w-full ${
+        elementsLoading ? 'flex items-center justify-center' : ''
+      }`}
+    >
+      {elementsLoading ? (
+        <LoadSpinner isBlocking={elementsLoading} />
+      ) : (
+        <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5'>
+          {elements}
+        </div>
+      )}
     </div>
   )
 }
