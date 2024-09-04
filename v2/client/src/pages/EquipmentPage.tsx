9e9228a2a9bf6ce4ed0bbc9397089b9bc1bc140d
Manraj Singh <manrajs2025@gmail.com>
2024-09-04 11:19:12 -0600

    Add equipment page card components

--- v2/client/src/pages/EquipmentPage.tsx
+++ v2/client/src/pages/EquipmentPage.tsx
@@ -1,3 +1,4 @@
+import { ItemCard } from '../components/cards/itemCard'
 import { Grid } from '../components/common/grid'
 import { PageHeader } from '../components/common/pageHeader'
 import { useFetchEquipment } from '../hooks/queries/useFetchEquipment'
@@ -5,7 +6,22 @@ import { useFetchEquipment } from '../hooks/queries/useFetchEquipment'
 export function EquipmentPage(): JSX.Element {
   const { equipment, loading } = useFetchEquipment()
 
-  console.log(equipment)
+  const cardElements = equipment.map((equip) => {
+    return (
+      <ItemCard
+        key={`equip-card-item-${equip.id}`}
+        baseDetails={{
+          ...equip
+        }}
+        extraDetails={{
+          equipmentProperties: {
+            attackDamage: equip.properties.attackDamage ?? undefined,
+            defense: equip.properties.defense ?? undefined
+          }
+        }}
+      />
+    )
+  })
 
   return (
     <div className='mt-4'>
@@ -14,7 +30,7 @@ export function EquipmentPage(): JSX.Element {
         subText='Weapons, shields, and outfits grant unique powers and strategies, essential for conquering the challenges of Hyrule.'
       />
       <div className='mt-4'>
-        <Grid elementsLoading={loading} elements={[]} />
+        <Grid elementsLoading={loading} elements={cardElements} />
       </div>
     </div>
   )
