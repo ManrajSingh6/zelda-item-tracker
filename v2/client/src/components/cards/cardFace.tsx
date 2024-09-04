563c10ee77c84c94a853f88fcdfca83352f44315
Manraj Singh <manrajs2025@gmail.com>
2024-09-04 11:18:24 -0600

    Update card face to show DLC items

--- v2/client/src/components/cards/cardFace.tsx
+++ v2/client/src/components/cards/cardFace.tsx
@@ -1,16 +1,24 @@
+import { CardTitle } from './cardTitle'
+
 interface CardFaceProps {
   readonly id: number
   readonly imageSrc: string
   readonly title: string
+  readonly isDlc: boolean
 }
 
-export function CardFace({ id, imageSrc, title }: CardFaceProps): JSX.Element {
+export function CardFace({
+  id,
+  imageSrc,
+  title,
+  isDlc
+}: CardFaceProps): JSX.Element {
   return (
     <>
       <img src={imageSrc} alt='card-item-img' className='rounded-lg' />
       <div>
         <p className='text-xs text-accentIndigo'>ID: {id}</p>
-        <h1 className='font-semibold capitalize text-accent'>{title}</h1>
+        <CardTitle title={title} isDlc={isDlc} />
       </div>
     </>
   )
