e6da3ed90fb2e074f1daa22237c12a01b9c81aaf
Manraj Singh <manrajs2025@gmail.com>
2024-09-06 23:13:52 -0600

    Add dropdown filter to equipment page and defaults

--- v2/client/src/pages/EquipmentPage.tsx
+++ v2/client/src/pages/EquipmentPage.tsx
@@ -5,16 +5,25 @@ import { PageHeader } from '../components/common/pageHeader'
 import { TextInput } from '../components/common/searchInput'
 import { useFetchEquipment } from '../hooks/queries/useFetchEquipment'
 import { useDebounce } from '../hooks/useDebounce'
+import { Dropdown, DropdownOption } from '../components/common/dropdown'
+import { ZERO } from '../utils/constants'
+import { DEFAULT_DROPDOWN_OPTS, NO_DROPDOWN_ITEMS } from '../utils/defaults'
 
 export function EquipmentPage(): JSX.Element {
   const { equipment, loading } = useFetchEquipment()
 
+  const [dropdownDisplayCount, setDropdownDisplayCount] =
+    useState<DropdownOption>(getDefaultDropdownOption())
+
   const [searchTerm, setSearchTerm] = useState('')
+
   const { debouncedValue: debouncedSearchTerm } = useDebounce({
     value: searchTerm
   })
 
   const cardElements = equipment
+    // Can assert type because dropdown values in this context are numbers
+    .slice(ZERO, dropdownDisplayCount.value as number)
     .filter((item) =>
       item.name.toLowerCase().includes(debouncedSearchTerm.toLowerCase())
     )
@@ -35,17 +44,39 @@ export function EquipmentPage(): JSX.Element {
       )
     })
 
+  function handleDropdownChange(option: DropdownOption | null): void {
+    if (option) {
+      setDropdownDisplayCount(option)
+    }
+  }
+
+  function getDefaultDropdownOption(): DropdownOption {
+    return {
+      label: 'All',
+      value: loading ? NO_DROPDOWN_ITEMS : equipment.length
+    }
+  }
+
   return (
     <div className='mt-4 flex flex-col gap-4'>
       <PageHeader
         header='Equipment'
         subText='Weapons, shields, and outfits grant unique powers and strategies, essential for conquering the challenges of Hyrule.'
       />
-      <TextInput
-        label='Search Equipment'
-        value={searchTerm}
-        onChange={(input) => setSearchTerm(input)}
-      />
+      <div className='flex flex-row justify-between gap-2'>
+        <TextInput
+          label='Search Equipment'
+          value={searchTerm}
+          onChange={(input) => setSearchTerm(input)}
+        />
+        <Dropdown
+          dropdownLabel='Display'
+          selectedOption={dropdownDisplayCount}
+          defaultOption={getDefaultDropdownOption()}
+          options={DEFAULT_DROPDOWN_OPTS}
+          onSelect={handleDropdownChange}
+        />
+      </div>
       <Grid elementsLoading={loading} elements={cardElements} />
     </div>
   )
---
+++ v2/client/src/utils/defaults.ts
@@ -0,0 +1,12 @@
+import { DropdownOption } from '../components/common/dropdown'
+
+export const NO_DROPDOWN_ITEMS = -1
+
+export const DEFAULT_DROPDOWN_OPTS: readonly DropdownOption[] = [
+  { label: '5', value: 5 },
+  { label: '10', value: 10 },
+  { label: '15', value: 15 },
+  { label: '20', value: 20 },
+  { label: '50', value: 50 },
+  { label: '100', value: 100 }
+]
